cc664a0bfca54c6afc13d86c6cc0de50317fc9c3
Elias <eliasolderbakk@gmail.com>
2026-01-30 15:29:03 +0100

    updates

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        configurator: resolve(__dirname, 'configurator.html')
+        configurator: resolve(__dirname, 'configurator.html'),
+        'pakke-ekspedisjonstur': resolve(__dirname, 'pakke-ekspedisjonstur.html'),
+        'pakke-fokus-paa-vidda': resolve(__dirname, 'pakke-fokus-paa-vidda.html'),
+        'pakke-hotellet-for-dere': resolve(__dirname, 'pakke-hotellet-for-dere.html')
       }
     }
   }
